176bacb5a00897e494fb3b4b67232e45d359d163
유한승 <kern86@wemakeprice.com>
2021-01-07 01:40:15 +0900

    feat(add-layout): :sparkle:: Add layout components

---
+++ src/components/Layout.tsx
@@ -0,0 +1,45 @@
+import React, { FC } from 'react';
+import { Menu, Layout, Breadcrumb } from 'antd';
+import styled from 'styled-components';
+
+const { Header, Content, Footer } = Layout;
+
+const styles = {
+  container: {
+    padding: '0 50px',
+  },
+  ContentMain: styled.div`
+    min-height: 280px;
+    padding: 24px;
+    background: #ffffff;
+  `,
+};
+
+interface IProps {
+  children?: Element | Element[];
+}
+
+const PageLayout: FC<IProps> = ({ children }) => {
+  return (
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal">
+          <Menu.Item key="1">Home</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={styles.container}>
+        <Breadcrumb>
+        </Breadcrumb>
+        <styles.ContentMain>
+          {children}
+        </styles.ContentMain>
+      </Content>
+      <Footer>
+        Hi! It's test footer
+      </Footer>
+    </Layout>
+  );
+};
+
+export default PageLayout;
