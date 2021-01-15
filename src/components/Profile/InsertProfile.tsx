177a920ea263c6bbf3c2f77b0dbc83d9fb333df0
유한승 <trustyoo86@gmail.com>
2021-01-15 18:21:26 +0900

    feat(add-recoil-profile): :sparkle: Add recoil profile + query atom
    * query atom 추가
    * recoil profile 동적 바인딩 추가

--- src/components/Profile/InsertProfile.tsx
+++ src/components/Profile/InsertProfile.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import { Form, Input } from 'antd';
+import { useRecoilState } from 'recoil';
 
-const InsertProfile = () => {
+import { queryAtom } from '@recoils/profile';
+
+function InsertProfile() {
+  const [query, setQuery] = useRecoilState(queryAtom);
+
+  const onChange = ({ target }) => {
+    const val = target.value;
+    setQuery(val);
+  };
+
   return (
     <Form>
       <Form.Item
         label="Username"
         name="username">
-        <Input placeholder="Username 입력" />
+        <Input
+          placeholder="Username 입력"
+          value={query}
+          onChange={onChange} />
       </Form.Item>
     </Form>
   );
-};
+}
 
 export default InsertProfile;
--- src/recoils/profile.ts
+++ src/recoils/profile.ts
@@ -1,12 +1,18 @@
-import { selector } from 'recoil';
+import { atom, selector, ReadWriteSelectorOptions } from 'recoil';
 import axios from 'axios';
 
 const PREFIX = 'PROFILE';
 
+export const queryAtom = atom({
+  key: `${PREFIX}/ATOM/QUERY`,
+  default: '',
+});
+
 export const profileSelector = selector({
   key: `${PREFIX}/PROFILE`,
-  get: async () => {
-    const res = await axios.get('https://api.github.com/search/repositories?q=q');
+  get: async ({ get }) => {
+    const query = get(queryAtom) || 'q';
+    const res = await axios.get(`https://api.github.com/search/repositories?q=${query}`);
     return res.data;
   },
 });
